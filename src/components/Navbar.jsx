1ea7a790d352edc1db992952afe776f9c4050e83
Nitiwat0711 <nitiwat.a@ku.th>
2021-08-06 16:40:47 +0700

    customize hamburger bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { Button, Drawer, Menu } from "antd";
+import { MenuOutlined } from "@ant-design/icons";
+import "../styles/navbar.css";
 
 class Navbar extends Component {
+  state = {
+    drawerVisible: false,
+  };
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -9,17 +15,53 @@ class Navbar extends Component {
           <Link className="navbar-brand p-1" to="/">
             Combuy Career
           </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+
+          <Button
+            type="text"
+            className="menuButtonMobile"
+            style={{
+              color: "#fff",
+              fontSize: "16px",
+            }}
+            onClick={() => this.setState({ drawerVisible: true })}
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+            <MenuOutlined
+              style={{ display: "inline-block", verticalAlign: "middle" }}
+            />
+          </Button>
+
+          <Drawer
+            title="เมนู"
+            placement="right"
+            closable={true}
+            onClose={() => this.setState({ drawerVisible: false })}
+            visible={this.state.drawerVisible}
+            width={"90%"}
+          >
+            <Menu mode="inline">
+              <Menu.Item
+                key="alljob"
+                onClick={() => this.setState({ drawerVisible: false })}
+              >
+                <span>ตำแหน่งงานทั้งหมด</span>
+                <Link to="/allJob" />
+              </Menu.Item>
+              <Menu.Item
+                key="internship"
+                onClick={() => this.setState({ drawerVisible: false })}
+              >
+                <span>ฝึกงานกับเรา</span>
+                <Link to="/internship" />
+              </Menu.Item>
+              <Menu.Item
+                key="cantactUs"
+                onClick={() => this.setState({ drawerVisible: false })}
+              >
+                <span>ติดต่อเรา</span>
+                <Link to="/contact" />
+              </Menu.Item>
+            </Menu>
+          </Drawer>
 
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
